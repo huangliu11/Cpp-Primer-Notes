07684f2e181135ac99449b5cea2b66e11bbbb59c
huangliu11 <71953432+huangliu11@users.noreply.github.com>
2022-02-05 16:57:15 +0800

    Update Sales_data.h

--- Sales_data.h
+++ Sales_data.h
@@ -1,10 +1,10 @@
 #ifndef SALES_DATA_H
 #define SALES_DATA_H
 #include<iostream>
-using namespace std;
+//using namespace std;    //在头文件中尽量不要使用using
 struct Sales_data {
-	string bookNo;
-	string bookName;
+	std::string bookNo;
+	std::string bookName;
 	unsigned units_sold = 0;
 	double revenue = 0.0;
 };
